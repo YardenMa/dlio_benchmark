7422d6ae15eefdf8efd43b6cef1f7c368fc5b68c
Shay Sirek <shay.sirek@volumez.com>
2024-08-01 13:16:29 +0300

    NPZReader - replace dim clipping by lower bound 1

--- dlio_benchmark/reader/npz_reader.py
+++ dlio_benchmark/reader/npz_reader.py
@@ -42,7 +42,7 @@ class NPZReader(FormatReader):
         mean_dim = 12107
         std_dim = 2822
         current_dim = np.random.normal(mean_dim, std_dim, 2)
-        current_dim = np.clip(current_dim, mean_dim - (3 * std_dim), mean_dim + (3 * std_dim))
+        current_dim[current_dim < 1] = 1
         file_size = current_dim[0] * current_dim[1]
 
         time_to_sleep = file_size / read_speed_gb_per_sec
